ef24fb803e49bcb6bdc86f114748ec808edb2591
coffee <164589895@qq.com>
2021-12-09 16:34:58 +0800

    1

--- src/ExcelIn.php
+++ src/ExcelIn.php
@@ -2,6 +2,8 @@
 
 namespace Phpfunction\Excel;
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
 /**
  * Excel导出
  * Class ExcelOut
@@ -21,11 +23,37 @@ class ExcelIn
 
 
     /**
-     * 样式——对齐
-     * @return array[]
+     * 数据列表
      */
-    private function exe()
+    public function getData()
     {
+        $file_name = $this->file;
+        $type      = pathinfo($file_name);
+        $type      = strtolower($type["extension"]);
+
+        if ("xlsx" == $type) {
+            $objReader = IOFactory::createReader("Xlsx");
+        } elseif ("xls" == $type) {
+            $objReader = IOFactory::createReader("Xls");
+        } else {
+            return "文件类型不正确";
+        }
+
+        $objPHPExcel   = $objReader->load($file_name);
+        $sheet         = $objPHPExcel->getSheet(0);
+        $highestRow    = $sheet->getHighestRow();    // 取得总行数
+        $highestColumn = $sheet->getHighestColumn(); // 取得总列数
+        $data          = [];
+
+        for ($i = 1; $i <= $highestRow; $i++) {
+            $tmp      = [];
+            $tmp["A"] = $sheet
+                ->getCell("A" . $i)
+                ->getCalculatedValue();
+            $data[]   = $tmp;
+        }
+
+        return $data;
     }
 
 
